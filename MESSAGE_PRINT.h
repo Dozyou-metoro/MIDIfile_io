fc33b4c42d9c24dee36810e0de587ce9c6d904b3
Dozyou-metoro <dozyoumetoro_0000@outlook.jp>
2023-12-27 06:12:51 +0000

    音階周波数対応表を実装

--- MESSAGE_PRINT.h
+++ MESSAGE_PRINT.h
@@ -5,6 +5,6 @@
 #include<cerrno>
 #include<cstring>
 
-#define ERROR_PRINT(str,no) printf("\nERROR:%s\nerrno:%d\nMESSAGE:%s\nFILE:%s::%s(),<LINE:%d>\nRETURN_NUM:%d",strerror(errno),errno,str,__FILE__,__FUNCTION__,__LINE__,no);fflush(stdout);exit(no);
+#define ERROR_PRINT(str,no) printf("\nERROR:%s\nerrno:%d\nMESSAGE:%s\nFILE:%s::%s(),<LINE:%d>\nRETURN_NUM:%d",strerror(errno),errno,str,__FILE__,__FUNCTION__,__LINE__);fflush(stdout);exit(no);
 
 #endif 
---
+++ tone_name.hpp
@@ -0,0 +1,161 @@
+#ifndef tone_name
+#define tone_name
+
+#include<array>
+#include<cmath>
+
+enum
+{
+    C_1,
+    C_s1,
+    D_1,
+    D_s1,
+    E_1,
+    F_1,
+    F_s1,
+    G_1,
+    G_s1,
+    A_1,
+    A_s1,
+    B_1,
+    C0,
+    Cs0,
+    D0,
+    Ds0,
+    E0,
+    F0,
+    Fs0,
+    G0,
+    Gs0,
+    A0,
+    As0,
+    B0,
+    C1,
+    Cs1,
+    D1,
+    Ds1,
+    E1,
+    F1,
+    Fs1,
+    G1,
+    Gs1,
+    A1,
+    As1,
+    B1,
+    C2,
+    Cs2,
+    D2,
+    Ds2,
+    E2,
+    F2,
+    Fs2,
+    G2,
+    Gs2,
+    A2,
+    As2,
+    B2,
+    C3,
+    Cs3,
+    D3,
+    Ds3,
+    E3,
+    F3,
+    Fs3,
+    G3,
+    Gs3,
+    A3,
+    As3,
+    B3,
+    C4,
+    Cs4,
+    D4,
+    Ds4,
+    E4,
+    F4,
+    Fs4,
+    G4,
+    Gs4,
+    A4,
+    As4,
+    B4,
+    C5,
+    Cs5,
+    D5,
+    Ds5,
+    E5,
+    F5,
+    Fs5,
+    G5,
+    Gs5,
+    A5,
+    As5,
+    B5,
+    C6,
+    Cs6,
+    D6,
+    Ds6,
+    E6,
+    F6,
+    Fs6,
+    G6,
+    Gs6,
+    A6,
+    As6,
+    B6,
+    C7,
+    Cs7,
+    D7,
+    Ds7,
+    E7,
+    F7,
+    Fs7,
+    G7,
+    Gs7,
+    A7,
+    As7,
+    B7,
+    C8,
+    Cs8,
+    D8,
+    Ds8,
+    E8,
+    F8,
+    Fs8,
+    G8,
+    Gs8,
+    A8,
+    As8,
+    B8,
+    C9,
+    Cs9,
+    D9,
+    Ds9,
+    E9,
+    F9,
+    Fs9,
+    G9,
+};
+
+class tone_name_array
+{
+private:
+    
+public:
+    tone_name_array();
+    ~tone_name_array();
+
+    std::array<double,G9 + 1> tone_frequency;
+};
+
+tone_name_array::tone_name_array()
+{
+    for(int i=0;i<G9 + 1;i++){
+        tone_frequency[i]=(32.703/4) * pow(pow(2, 1.0 / 12.0), i);
+    }
+}
+
+tone_name_array::~tone_name_array()
+{
+}
+
+#endif
